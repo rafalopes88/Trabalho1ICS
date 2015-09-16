cfdf888c1ab91c79cd2ff9e4c8baf52a743b7d00
rafalopes88 <alvaresrafa@hotmail.com>
2015-09-15 21:36:32 -0300

    Commit inicial

--- main.java
+++
@@ -1,9 +0,0 @@
- public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                UIManager.put("swing.boldMetal", Boolean.FALSE); 
-                createAndShowGUI();
-            }
-        });
-    }
-}
