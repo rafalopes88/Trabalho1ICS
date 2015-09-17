86ff86a77ac6237fffcfe79d34c3ca835b436256
rafalopes88 <alvaresrafa@hotmail.com>
2015-09-17 12:04:49 -0300

    botoes de play, pause e stop

--- TocadorMid.java
+++ TocadorMid.java
@@ -1,57 +1,243 @@
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.text.DecimalFormat;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JSlider;
+import javax.swing.JProgressBar;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.UIManager;
 import javax.swing.SwingUtilities;
-import javax.swing.filechooser.*;
+
+import java.awt.Dimension;
+import java.awt.Container;
+import java.awt.Insets;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.InvalidMidiDataException;
 
 public class TocadorMid extends JPanel implements ActionListener {
-    JButton botaoAbrir;
-    JTextArea log;
+
+    private  int largura = 390;
+    private  int altura  = 280;
+    private long inicio = 0;
+
+    JButton botaoAbrir, botaoTocar, botaoPausar, botaoParar;
+    JTextField caminhoArq;
     JFileChooser pa;
+
+    private boolean soando;
+
+    private Sequencer  sequenciador = null;
+    private Sequence   sequencia;
+    private Receiver   receptor;
 
     public TocadorMid() {
         super(new BorderLayout());
  
-        log = new JTextArea(5,20);
+        /*log = new JTextArea(5,20);
         log.setMargin(new Insets(5,5,5,5));
         log.setEditable(false);
-        JScrollPane painelLog = new JScrollPane(log);
- 
-        //Create a file chooser
+        JScrollPane painelLog = new JScrollPane(log);*/
+        JPanel painelLog = new JPanel();
+        caminhoArq = new JTextField();
+        caminhoArq.setText(System.getProperty("user.dir"));
+        caminhoArq.setEditable(false);
+        painelLog.add(caminhoArq);
+
+
         pa = new JFileChooser();
- 
-        botaoAbrir = new JButton("abrir");
-                                
+        botaoAbrir = new JButton("Abrir");
+        botaoTocar = new JButton("Play");
+        botaoPausar = new JButton("Pause");
+        botaoParar = new JButton("Stop");
+
         botaoAbrir.addActionListener(this);
- 
+        botaoTocar.addActionListener(this);
+        botaoPausar.addActionListener(this);
+        botaoParar.addActionListener(this);
         
+        botaoTocar.setEnabled(false);
+        botaoPausar.setEnabled(false);
+        botaoParar.setEnabled(false);
  
         
         JPanel botaoPainel = new JPanel(); 
         botaoPainel.add(botaoAbrir);
+        botaoPainel.add(botaoTocar);
+        botaoPainel.add(botaoPausar);
+        botaoPainel.add(botaoParar);
         
  
-        //adicionando o botao e log a este painel
-        add(botaoPainel, BorderLayout.PAGE_START);
+        //adicionando o botao e log aos paineis painel
         add(painelLog, BorderLayout.CENTER);
+        add(botaoPainel, BorderLayout.PAGE_END);
+        
     }
+    public void Abrir(){
+        JFileChooser selecao = new JFileChooser(".");  
+        selecao.setFileSelectionMode(JFileChooser.FILES_ONLY);              
+        selecao.setFileFilter(new FileFilter() {
+            public boolean accept(File f){
+                if (!f.isFile()) 
+                    return false;
+                   
+                String name = f.getName().toLowerCase();
+                    
+                return name.endsWith(".mid") || name.endsWith(".midi");
+                    
+            }
+
+            public String getDescription(){
+
+                return "Arquivo Midi (*.mid,*.midi)";
+            }
+        });
+
+        selecao.showOpenDialog(this);  
+
+        if(selecao.getSelectedFile() != null){
+            caminhoArq.setText(selecao.getSelectedFile().toString());  
+            File arqseqnovo = selecao.getSelectedFile();
+            try { 
+                if(sequenciador!=null && sequenciador.isRunning()) { 
+                    sequenciador.stop();
+                    sequenciador.close();
+                    sequenciador = null;
+                }
+                Sequence sequencianova = MidiSystem.getSequence(arqseqnovo);           
+                double duracao = sequencianova.getMicrosecondLength()/1000000.0d;
+                 
+                //botaoMOSTRADORarquivo.setText("Arquivo: \"" + arqseqnovo.getName() + "\"");                
+                //botaoMOSTRADORduracao.setText("\nDura\u00e7\u00e3o:"+ formataInstante(duracao));                   
+                  
+                botaoTocar.setEnabled(true);
+                botaoPausar.setEnabled(false);
+                botaoParar.setEnabled(false);                                    
+            }
+            catch (Throwable e1) { 
+                System.out.println("Erro em carregaArquivoMidi: "+ e1.toString());
+            }
+        }
+    }
+
+    public void Tocar(String caminho){
+        try{
+            File arqmidi = new File(caminho);
+            sequencia    = MidiSystem.getSequence(arqmidi);  
+            sequenciador = MidiSystem.getSequencer();  
+
+            sequenciador.setSequence(sequencia); 
+            sequenciador.open();  
+            Thread.sleep(500);
+            sequenciador.start();  
+            
+            receptor = sequenciador.getTransmitters().iterator().next().getReceiver();
+            sequenciador.getTransmitter().setReceiver(receptor);
+         
+            //botaoMOSTRADORarquivo.setText("Arquivo: \"" + arqmidi.getName() + "\"");
+                                                   
+            long duracao  = sequencia.getMicrosecondLength()/1000000;
+            //botaoMOSTRADORduracao.setText("\nDura\u00e7\u00e3o:"+ formataInstante(duracao)); 
+            //botaoMOSTRADORinstante.setText(formataInstante(0));                
+                                            
+            sequenciador.setMicrosecondPosition(inicio);
+
+            if (sequenciador.isRunning()){ 
+                duracao = sequenciador.getMicrosecondLength();
+                soando = true;
+            } 
+            else { 
+                soando = false; 
+                sequenciador.stop();  
+                sequenciador.close();
+                inicio = 0L;
+                duracao = 0;
+            }  
+            
+             botaoAbrir.setEnabled(false);
+             botaoTocar.setEnabled(false);
+             botaoPausar.setEnabled(true);
+             botaoParar.setEnabled(true);                
+                
+        }
+        catch(MidiUnavailableException e1) { 
+            System.out.println(e1+" : Dispositivo midi nao disponivel.");
+        }
+        catch(InvalidMidiDataException e2) { 
+            System.out.println(e2+" : Erro nos dados midi."); 
+        }
+        catch(IOException e3) { 
+            System.out.println(e3+" : O arquivo midi nao foi encontrado.");   
+        }
+        catch(Exception e){  
+            System.out.println(e.toString());  
+        }  
+    }
+
+    public void Pausar(){
+
+            inicio = sequenciador.getMicrosecondPosition();
+            soando = false;
+            sequenciador.stop();  
+            
+            botaoAbrir.setEnabled(false);            
+            botaoTocar.setEnabled(true);
+            botaoPausar.setEnabled(false);
+            //botaoPARAR.setEnabled(false);            
+    }
+
 
 
     public void actionPerformed(ActionEvent e) {
  
         if (e.getSource() == botaoAbrir) {
-            int valorRetorno = pa.showOpenDialog(TocadorMid.this);
- 
-            if (valorRetorno == JFileChooser.APPROVE_OPTION) {
-                File arq = pa.getSelectedFile();
-                //Operaçao de abrir o arquivo
-                log.append("Abrindo: " + arq.getName() + ".\n" );
-            } else {
-                log.append("Operacao de abrir cancelada pelo o usuario.\n");
-            }
-            log.setCaretPosition(log.getDocument().getLength());
+            Abrir();
         }
+        else if(e.getSource() == botaoTocar){
+            Tocar(caminhoArq.getText());
+        }
+        else if(e.getSource() == botaoPausar){
+            Pausar();
+        }
+        else if(e.getSource() == botaoParar){
+            Parar();
+        }
+    }
+
+    public void Parar() {
+            soando = false;
+            sequenciador.stop();  
+            sequenciador.close();
+            sequenciador = null;
+            inicio = 0L;
+            
+             botaoAbrir.setEnabled(true);            
+             botaoTocar.setEnabled(true);
+             botaoPausar.setEnabled(false);
+             botaoParar.setEnabled(false);
+            
+             //sliderPROGRESSOinstante.setValue(0);             
+             //botaoMOSTRADORinstante.setText(formataInstante(0));      
     }
 
 
